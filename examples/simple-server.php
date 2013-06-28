e51e702c8f74b6c2e66d18dfc429abd5b6160d25
alban <alban@octopuce.fr>
2013-06-28 16:27:14 +0200

    [enh] Comment simple server example

--- examples/simple-server.php
+++ examples/simple-server.php
@@ -1,14 +1,15 @@
 <?php
 
-/** Implements a simple response upload server for the omk client
-  * To test this example, you must have a valid upload and final location setup
-  * 
-  * 
-  */ 
+/** 
+ * Implements a simple response upload server for the omk client
+ * To test this example, you must have a valid upload and final location setup
+ * This example is unsafe: it uses a single user that is an admin
+ * 
+ */ 
 
+// TODO : SQL Script 
 
-
-// set up include path and autoload
+// Sets up include path and autoload
 $old_include_path = get_include_path(); 
 set_include_path(dirname(__FILE__)."/../src".PATH_SEPARATOR.$old_include_path);
 function __autoload($className) {
@@ -32,38 +33,44 @@ if( ! session_start() ){
 }
 $_SESSION["is_admin"] = TRUE;
 
-// instanciate a dummy authentification adapter
+// Instanciates a dummy authentification adapter
 $authentificationAdapter = new OMK_Authentification_Session(array(
     
 ));
 
+// Instanciates a mysql authentification adapter
 $databaseAdapter = new OMK_Database_Mysql(array(
-    "host"          => "localhost",
-    "database"      => "omk",
-    "user"          => "omk",
-    "password"      => "omk",
-    "prefix"        => ""
+    "host"          => "localhost", // ! You might want to modify that
+    "database"      => "omk", // ! You might want to modify that
+    "user"          => "omk", // ! You might want to modify that
+    "password"      => "omk", // ! You might want to modify that
+    "prefix"        => "" // ! You might want to modify that
 ));
 
+// Instanciates a single folder file storage adapter
 $fileAdapter = new OMK_File_SingleFolder(array(
-    "storage_path"  => "/home/alban/code/omkstorage",
-    "file_path"     => "http://omk53storage.octopuce.fr"
+    "storage_path"  => "/home/alban/code/omkstorage", // ! You might want to modify that
+    "file_path"     => "http://omk53storage.octopuce.fr" // ! You might want to modify that
 ));
 
+// Instanciates a single folder upload adapter
 $uploadAdapter = new OMK_Upload_SingleFolder(array(
-    "tmp_path"      => "/home/alban/code/omkstorage",
-    "name"          => "singleFolder"
+    "tmp_path"      => "/home/alban/code/omkstorage", // ! You might want to modify that
+    "name"          => "singleFolder" 
 ));
 
+// Instanciates a file logging adapter
 $loggerAdapter  = new OMK_Logger_File(array(
    "level"          => OMK_Logger_Adapter::DEBUG, 
-   "log_file_path"  => "/tmp/omk.log" 
+   "log_file_path"  => "/tmp/omk.log"  // ! You might want to modify that
 ));
 
+// Instanciates a dummy translation adapter
 $translationAdapter = new OMK_Translation_Dummy(array(
     
 ));
 
+// Defines a not really comprehensive whitelist for audio/video media
 $mimeTypeWhitelist = array(
     "audio/basic",
     "audio/L24",
@@ -93,16 +100,20 @@ $mimeTypeWhitelist = array(
     "video/x-flv",
 );
 
+// Sets your client server URL, by default the current script
+$client_url                     = (strstr( $_SERVER["SERVER_PROTOCOL"], "HTTP/") ? "http":"https")."://{$_SERVER["SERVER_NAME"]}{$_SERVER["SCRIPT_NAME"]}";
 
-        
-// set up the client
+// Sets the transcoder URL, by default our free one
+$transcoder_url                 = "http://omkt.octopuce.fr/api";
+
+// Sets up the client with all the dependancies defined
 $client = new OMK_Client(array(
     "lang"                      => "fr",
     "application_name"          => "simple-server-example",
-    "client_key"             => "1234567890abcdef",
-    "client_url"             => (strstr( $_SERVER["SERVER_PROTOCOL"], "HTTP/") ? "http":"https")."://{$_SERVER["SERVER_NAME"]}{$_SERVER["SCRIPT_NAME"]}",
-    "transcoder_key"        => "9ef121fe8bff86a8764bae831d68f804",
-    "transcoder_url"        => "http://omkt.octopuce.fr/api",
+    "client_key"                => "1234567890abcdef",
+    "client_url"                => $client_url,
+    "transcoder_key"            => "9ef121fe8bff86a8764bae831d68f804",
+    "transcoder_url"            => $transcoder_url,
     "config_file"               => __FILE__,
     "css_url_path"              => (strstr( $_SERVER["SERVER_PROTOCOL"], "HTTP/") ? "http":"https")."://{$_SERVER["SERVER_NAME"]}".dirname($_SERVER["SCRIPT_NAME"])."/../src/OMK/views/css",
     "js_url_path"               => (strstr( $_SERVER["SERVER_PROTOCOL"], "HTTP/") ? "http":"https")."://{$_SERVER["SERVER_NAME"]}".dirname($_SERVER["SCRIPT_NAME"])."/../src/OMK/views/js",
@@ -116,19 +127,23 @@ $client = new OMK_Client(array(
     "uploadAdapter"             => $uploadAdapter
 ));
 
+// This client does some archaic routing 
+
+// Checks if an action was requested
 $action = array_key_exists("action", $_REQUEST) ? $_REQUEST["action"] : NULL ;
 
-// Render (html)
+// Renders a view if no action requested
 if( NULL == $action ){
     if (array_key_exists("view", $_REQUEST) && NULL != $_REQUEST["view"]) {
         $view = $_REQUEST["view"];
     } else {
+        // By default, render the upload view
         $view = "upload";
     }
     die($client->render($view));
 }
 
-// Respond (json)
+// Responds with json to action calls 
 $response = $client->call(array(
     "action"    => $action,
     "format"    => "json"
